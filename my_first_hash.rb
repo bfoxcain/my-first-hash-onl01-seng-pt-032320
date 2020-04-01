827e236ffeb40b9ee50a5f9c04d212d983e6576b
Beverly Appleton <bfcain19@gmail.com>
2020-04-01 23:59:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,9 +4,9 @@ def my_hash
 end
 
 
-def shipping_manifest(the_manifest)
-  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-puts the_manifest
+def shipping_manifest
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+
 
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
