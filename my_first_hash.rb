023f3f10812b2c397c7746cef8d3c549415d9406
Beverly Appleton <bfcain19@gmail.com>
2020-04-01 23:49:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,9 +1,6 @@
 def my_hash
   my_hash = {"name" => "Fox", "age" => "38"}
-end
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-
 end
 
 
